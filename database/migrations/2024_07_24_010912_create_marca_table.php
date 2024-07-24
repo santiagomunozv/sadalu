b799ab381fe08a7a5eac0f0f359c587d6fd6c96e
Santiago Muñoz <santiagomunozviana@gmail.com>
2024-07-23 20:19:46 -0500

    Marca migration and seed

---
+++ database/migrations/2024_07_24_010912_create_marca_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marca', function (Blueprint $table) {
+            $table->bigIncrements('idMarca')->comment('Id');
+            $table->string('nombreMarca');
+            $table->string('estadoMarca');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marca');
+    }
+};
